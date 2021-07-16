a6518033e829b7d4c4a315d10f6d8fc58b4835ef
Deshdeepak <rkdeshdeepak1@gmail.com>
2021-07-16 15:51:24 +0530

    Play jw video

--- webapp/main.py
+++ webapp/main.py
@@ -11,11 +11,13 @@ bc_url = f"https://edge.api.brightcove.com/playback/v1/accounts/{ACCOUNT_ID}/vid
 
 bc_hdr = {"BCOV-POLICY": BCOV_POLICY}
 
+jw_url = "https://cdn.jwplayer.com/v2/media"
+
 app = Flask(__name__)
 
 
 @app.route("/<int(fixed_digits=13):video_id>")
-def index(video_id):
+def brightcove(video_id):
     video_response = requests.get(f"{bc_url}/{video_id}", headers=bc_hdr)
 
     if video_response.status_code != 200:
@@ -31,13 +33,38 @@ def index(video_id):
     microsoft_url = ""
     if "key_systems" in video_source:
         widevine_url = video_source["key_systems"]["com.widevine.alpha"]["license_url"]
-        microsoft_url = video_source["key_systems"]["com.microsoft.playready"]["license_url"]
+        microsoft_url = video_source["key_systems"]["com.microsoft.playready"][
+            "license_url"
+        ]
     track_url = video["text_tracks"][1]["src"]
     return render_template(
         "template.html",
+        type="brightcove",
         video_name=video_name,
         video_url=video_url,
         track_url=track_url,
         widevine_url=widevine_url,
         microsoft_url=microsoft_url,
+    )
+
+
+@app.route("/<string(length=8):video_id>")
+def jw(video_id):
+    video_response = requests.get(f"{jw_url}/{video_id}")
+
+    if video_response.status_code != 200:
+        return "<font color=red size=20>Wrong Video ID</font>"
+
+    video = video_response.json()
+
+    video_name = video["title"]
+
+    video_url = video["playlist"][0]["sources"][0]["file"]
+    track_url = video["playlist"][0]["tracks"][0]["file"]
+    return render_template(
+        "template.html",
+        type="jw",
+        video_name=video_name,
+        video_url=video_url,
+        track_url=track_url,
     )
